f4e3006707d6f21052b88d04e570fafa46d5c55f
OLIVER  HSU <s122672103@gmail.com>
2020-02-26 12:16:20 +0800

    建模、狀態列、介面動畫

---
+++ Unity Project Shift Lightball/Assets/Script/BarControler.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class BarControler : MonoBehaviour
+{
+    public Image imgHp;        // 血條
+    public Image imgBattery;   // 電量條
+    public Image imgEnergy;    // 能量條
+ 
+
+
+
+
+    /// <summary>
+    /// 更新血條圖片長度
+    /// </summary>
+    /// <param name="hpMax"></param>
+    /// <param name="hpCurrent"></param>
+    public void UpdateHpBar(float hpMax, float hpCurrent)
+
+    {
+        imgHp.fillAmount = hpCurrent / hpMax;  //圖片.填滿數值 = 目前血量/最大血量
+       
+    }
+
+
+    /// <summary>
+    /// 更新電量條圖片長度
+    /// </summary>
+    /// <param name="batteryMax"></param>
+    /// <param name="batteryCurrent"></param>
+    public void UpdateBatteryBar(float batteryMax, float batteryCurrent)
+
+    {
+        imgBattery.fillAmount = batteryCurrent / batteryMax;  //圖片.填滿數值 = 目前電量/最大電量
+
+    }
+
+    /// <summary>
+    /// 更新能量條圖片長度
+    /// </summary>
+    /// <param name="energyMax"></param>
+    /// <param name="energyCurrent"></param>
+    public void UpdateEnergyBar(float energyMax, float energyCurrent)
+
+    {
+        imgEnergy.fillAmount = energyCurrent / energyMax;  //圖片.填滿數值 = 目前能量/最大能量
+
+    }
+
+
+
+
+
+
+}
---
+++ Unity Project Shift Lightball/Assets/Script/ButtonManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+
+
+     public GameObject basicMovement;
+     public GameObject basicMovement2;
+
+    public void ShowBasicMovement1()
+    {
+        basicMovement.SetActive(true);
+        basicMovement2.SetActive(false);
+
+    }
+
+
+    public void ShowBasicMovement2()
+    {
+        basicMovement.SetActive(false);
+        basicMovement2.SetActive(true);
+
+    }
+
+    public void CloseBasicMovement2()
+    {
+      
+        basicMovement2.SetActive(false);
+
+    }
+}
